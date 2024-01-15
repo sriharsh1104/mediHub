7b6a0296855a8f1ce5c25ae14ac90389c94ce519
sriharsh1104 <prashantsriharsh1104@gmail.com>
2024-01-15 17:42:42 +0530

    dashboard api

--- src/component/Dasboard/Dashboard.tsx
+++ src/component/Dasboard/Dashboard.tsx
@@ -1,19 +1,20 @@
-import React,{Dispatch} from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+import React, { Dispatch } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 // import "./Dashboard.scss";
-import CommanHeader from '../commanHeader/CommanHeader';
-import { useDispatch, useSelector } from 'react-redux';
-import toaster from '../comman/Toast';
-import { getAllEmployeesForAdmin } from '../Api/Actions/user.action';
+import CommanHeader from "../commanHeader/CommanHeader";
+import { useDispatch, useSelector } from "react-redux";
+import toaster from "../comman/Toast";
+import { getAllEmployeesForAdmin } from "../Api/Actions/user.action";
 
-const validationSchema = Yup.object().shape({
-});
+const validationSchema = Yup.object().shape({});
 
 const Dashboard: React.FC = () => {
   const dispatch: Dispatch<any> = useDispatch();
-  const organizationId = useSelector((state: any) => state.userDataSlice?.companyId);
-  
+  const organizationId = useSelector(
+    (state: any) => state.userDataSlice?.companyId
+  );
+
   const formik = useFormik({
     initialValues: {
       companyId: organizationId, // Set companyId as the only initial value
@@ -26,13 +27,12 @@ const Dashboard: React.FC = () => {
 
   const handleRegister = async () => {
     try {
-      const result:any = await getAllEmployeesForAdmin({
-        companyId: formik.values.companyId
+      const result: any = await getAllEmployeesForAdmin({
+        companyId: formik.values.companyId,
       });
       console.log("first212", result);
 
       if (result?.status === 200) {
-
       } else {
         toaster.info("Registration Failed");
       }
@@ -48,7 +48,10 @@ const Dashboard: React.FC = () => {
           <CommanHeader />
 
           <div className="bg-white p-4 rounded-md shadow-md">
-            <label htmlFor="companyId" className="block text-xl font-semibold mb-2">
+            <label
+              htmlFor="companyId"
+              className="block text-xl font-semibold mb-2"
+            >
               Company ID
             </label>
             <input
@@ -63,7 +66,11 @@ const Dashboard: React.FC = () => {
           </div>
         </div>
 
-        <button onClick={handleRegister} type="submit" className="mt-4 bg-blue-500 text-white p-2 rounded-md">
+        <button
+          onClick={handleRegister}
+          type="submit"
+          className="mt-4 bg-blue-500 text-white p-2 rounded-md"
+        >
           Add Member
         </button>
       </form>
